26c403ce3a6d9c68fecae557fd22fc15e7ed1eb6
Yomide <ayomidekofoworola59@gmail.com>
2026-03-27 04:14:03 +1000

    Fix: vendor

--- src/app/vendor/layout.tsx
+++ src/app/vendor/layout.tsx
@@ -47,9 +47,6 @@ export default function VendorLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
 
-      {/* Global Navbar */}
-      <Navbar />
-
       <div className="flex flex-1">
 
         {/* Sidebar */}
