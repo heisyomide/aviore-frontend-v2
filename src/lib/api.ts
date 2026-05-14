f7cdbf3fec2e2176e5837ec57e80c84a6b086a65
Ayomide Kofoworola <ayomidekofoworola59@gmail.com>
2026-05-14 16:44:36 +1000

    Fix: auth  now

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -60,10 +60,13 @@ api.interceptors.response.use(
     const originalRequest = error.config;
 
     // ACCESS TOKEN EXPIRED
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry
-    ) {
+if (
+  error.response?.status === 401 &&
+  !originalRequest._retry &&
+  !originalRequest.url?.includes('/auth/refresh')
+)
+    
+    {
       if (isRefreshing) {
         return new Promise(function (resolve, reject) {
           failedQueue.push({ resolve, reject });
